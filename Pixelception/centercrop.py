1b5c70b48ec78a4a1b79dba918769714f0059395
Robert Fraser <fluffyxvi@gmail.com>
2017-02-22 20:02:28 +1000

    Moved crop to function

--- Pixelception/centercrop.py
+++ Pixelception/centercrop.py
@@ -1,13 +1,18 @@
 from PIL import Image
 
-img = Image.open( input('File: ') )
-w, h = img.size
-crop = 0
-if w > h:
-	xx = (w-h)/2
-	crop = img.crop( (xx, 0, xx+h, h) )
-else:
-	yy = (h-w)/2
-	crop = img.crop( (0, yy, w, yy+w) )
-
-crop.save( "cropped.png", "PNG" )
+def CropCenter( image ):
+	img = Image.open( image )
+	w, h = img.size
+	crop = 0
+	if w > h:
+		xx = (w-h)/2
+		crop = img.crop( (xx, 0, xx+h, h) )
+	else:
+		yy = (h-w)/2
+		crop = img.crop( (0, yy, w, yy+w) )
+	
+	crop.save( "cropped.png", "PNG" )
+	
+if __name__ == "__main__":
+	filename = input("Filename: ")
+	AverageImageFolder( filename )
